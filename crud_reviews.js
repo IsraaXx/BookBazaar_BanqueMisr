e5066078349d8fe0c8ff880c1a210bac93a54702
22-101114 <168547040+22-101114@users.noreply.github.com>
2025-07-24 19:41:41 +0300

    Add files via upload

---
+++ crud_reviews.js
@@ -0,0 +1,42 @@
+use('bookbazaar_reviews');
+
+// Create
+
+const create = db.Reviews.insertOne({
+  book_id: 106,
+  reviewer: "Fayez",
+  rating: 4,
+  comment: "Well-written and engaging.",
+  created_at: new Date()
+});
+printjson(create);
+
+
+
+// Read
+
+const Read = db.Reviews.find({ reviewer: "Bob" });
+printjson(Read);
+
+
+
+// UPDATE 
+
+const update = db.Reviews.updateOne(
+  { reviewer: "Bob" },
+  {
+    $set: {
+      rating: 4,
+      comment: "Updated: Better than I thought!"
+    }
+  }
+);
+printjson(update);
+
+
+
+// DELETE 
+
+const delete_result = db.Reviews.deleteOne({ reviewer: "Anna" });
+printjson(delete_result);
+
